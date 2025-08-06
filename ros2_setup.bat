a5fffdddf5905c8b8aefb0eba8d46d3ce77980b1
guilherme-pinel-prestserv_petro <guilherme.pinel.prestserv@petrobras.com.br>
2025-08-06 10:25:43 -0300

    Atualizações locais antes de integrar com o repositório remoto

--- ros2_setup.bat
+++ ros2_setup.bat
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 @echo off
 
 :: Iniciar RTI Connext DDS
@@ -18,3 +19,21 @@ echo ROS 2 environment initialized. ROS_DOMAIN_ID is set to %ROS_DOMAIN_ID%.
 
 :: Manter a janela do CMD aberta
 cmd
+=======
+@echo off
+
+:: Iniciar RTI Connext DDS
+call "C:\Program Files\rti_connext_dds-7.5.0\resource\scripts\rtisetenv_x64Win64VS2017.bat"
+
+:: Iniciar setup do ROS 2
+call C:\dev\ros2_humble\local_setup.bat
+
+:: Configurar o ROS_DOMAIN_ID
+set ROS_DOMAIN_ID=0
+
+:: Exibir uma mensagem indicando que o ambiente está pronto
+echo ROS 2 environment initialized. ROS_DOMAIN_ID is set to %ROS_DOMAIN_ID%.
+
+:: Manter a janela do CMD aberta
+cmd
+>>>>>>> 55ee050 (Atualizações locais antes de integrar com o repositório remoto)
